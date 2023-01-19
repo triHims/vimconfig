d708a72961161b4fd43f30b30bbcfb0df2271ac0
trihims <himanshu.16bcs1156@abes.ac.in>
2023-01-19 09:41:18 +0530

    jdk for linux setup

--- lua/config/lsp/settings/jdtls.lua
+++ lua/config/lsp/settings/jdtls.lua
@@ -2,7 +2,7 @@ local config = { ... }
 config.use_lombok_agent = true
 local server_available, jdtls = require('nvim-lsp-installer.servers').get_server('jdtls')
 
-vim.fn.setenv('WORKSPACE', '/Users/himansht/.cache/STSWorkspace/JDTLS')
+vim.fn.setenv('WORKSPACE', '/home/himanshu/.cache/STSWorkspace/JDTLS')
 
 -- this will fill in the correct 'cmd' to use
 
